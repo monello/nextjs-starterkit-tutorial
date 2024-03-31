e31efebcab4acaf992094eaac931baf305fbb3b0
Morne Louw <louw.morne@gmail.com>
2024-03-31 16:58:58 +0100

    postgress - prisma - knex - initial migration

---
+++ knexfile.ts
@@ -0,0 +1,51 @@
+require('dotenv').config({ path: '.env.local' });
+
+import type { Knex } from "knex";
+
+// Update with your config settings.
+
+const config: { [key: string]: Knex.Config; } = {
+  development: {
+    client: "postgres",
+    connection: {
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD
+    },
+  },
+
+  // staging: {
+  //   client: "postgresql",
+  //   connection: {
+  //     database: "my_db",
+  //     user: "username",
+  //     password: "password"
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: "knex_migrations"
+  //   }
+  // },
+
+  // production: {
+  //   client: "postgresql",
+  //   connection: {
+  //     database: "my_db",
+  //     user: "username",
+  //     password: "password"
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: "knex_migrations"
+  //   }
+  // }
+
+};
+
+module.exports = config;
