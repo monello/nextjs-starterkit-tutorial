3e7eebaa3304b79e46e79e56828efc9af10871b9
Morne Louw <louw.morne@gmail.com>
2024-03-31 18:03:56 +0100

    minor changes and notes

--- knexfile.ts
+++ knexfile.ts
@@ -7,11 +7,15 @@ import type { Knex } from "knex";
 const config: { [key: string]: Knex.Config; } = {
   development: {
     client: "postgres",
+    // connection: process.env.DATABASE_URL,
     connection: {
       database: process.env.DB_NAME,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD
     },
+    migrations: {
+      extension: "ts"
+    }
   },
 
   // staging: {
